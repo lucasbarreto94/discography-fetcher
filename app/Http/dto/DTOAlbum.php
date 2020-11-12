e46831b064c4ea342cfaaf522c6404181ee7f4e7
lucasbarreto94 <blucas994@gmail.com>
2020-11-12 17:49:02 -0300

    Album DTO

---
+++ app/Http/dto/DTOAlbum.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\dto;
+
+class DTOAlbum 
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public $name;
+    public $released;
+    public $tracks;
+    public $cover;
+    public function __construct()
+    {
+        //
+    }
+
+    //
+}
