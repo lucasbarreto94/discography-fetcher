87ecb56f514ae0a8a3cc6162759bccf78d69823f
lucasbarreto94 <blucas994@gmail.com>
2020-11-12 17:47:58 -0300

    Config file

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+    return array(
+        'baseRouteApi' => '/api/v1',
+        'baseUrlSpotifyApi' => 'https://api.spotify.com/v1/',
+        'urlLoginSpotify' => 'https://accounts.spotify.com/authorize',
+        'urlTokenSpotify' => 'https://accounts.spotify.com/api/token',
+        'redirectUriSpotify' => 'http://localhost/callback',
+        'clientId' => '468bd5223ff747f7bc22e1647c63fa89',
+        'clientSecret' => '68418f626e374d589ed2ed9f70cd3890'
+    );
+?>
